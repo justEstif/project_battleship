baf20d20e5859e130b20413929a4bb5f626fa732
justEstif <estifanosbeyene21@gmail.com>
2022-04-22 09:44:19 -0400

    completed psedocode and planning, removed flowchart file'

--- src/scripts/main.test.js
+++ src/scripts/main.test.js
@@ -1,9 +1,9 @@
-const { ShipFactory } = require("./main.js");
+const { PlayerFactory } = require('./main.js')
 import { it, expect, describe } from "@jest/globals";
-describe("make sure that ShipFactory returns a ship object", () => {
-  let obj = ShipFactory.makeShip(4);
-  it("should return a ship object with the expected properties and methods", () => {
-    let expectedObj = { "hit": [], "length": 4, "sunk": false };
-    expect(obj).toEqual(expectedObj);
-  });
-});
+describe('make sure that PlayerFactory return a Player object', () => {
+  let playerObj = PlayerFactory.makePerson();
+  it("should return a person object", () => {
+    let expectedObj = {}
+    expect(playerObj).toEqual(expectedObj)
+  })
+})
